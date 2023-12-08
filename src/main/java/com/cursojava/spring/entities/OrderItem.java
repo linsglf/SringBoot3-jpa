1cca41c1083c562ec9dda7dcdadb8fbc6a6bbac6
Gabriel Lins <101433324+linsglf@users.noreply.github.com>
2023-12-07 23:28:47 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/cursojava/spring/entities/OrderItem.java
@@ -0,0 +1,46 @@
+package com.cursojava.spring.entities;
+
+import com.cursojava.spring.entities.pk.OrderItemPK;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class OrderItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+}
