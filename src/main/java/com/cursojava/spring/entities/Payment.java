eec3b9fbd8017f2ee04f2159209a71c1dfbd291d
Gabriel Lins <101433324+linsglf@users.noreply.github.com>
2023-12-09 21:50:35 -0300

    Subtotal & Total methods

--- src/main/java/com/cursojava/spring/entities/Payment.java
+++ src/main/java/com/cursojava/spring/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.cursojava.spring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -24,5 +25,6 @@ public class Payment implements Serializable {
 
     @OneToOne
     @MapsId
+    @JsonIgnore
     private Order order;
 }
